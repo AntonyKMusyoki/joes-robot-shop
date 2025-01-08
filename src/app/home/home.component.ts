11b25303891f3a85acc74218b698e5ef5d0bc487
Antony Kyalo Musyoki <antony.kyalo@bookinghub.com>
2025-01-07 23:40:20 -0600

    Chapter 3 End/Creating Angular Components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bot-home',
+  templateUrl: './home.Component.html',
+  styleUrls: ['./home.Component.css']
+  // template: `
+  //   <p class="blue">
+  //     Inline Home Works!
+  //   </p>`,
+  // styles: [`
+  //   .blue {
+  //     color: blue;
+  //     }
+  //   `]
+})
+export class HomeComponent {
+
+}
